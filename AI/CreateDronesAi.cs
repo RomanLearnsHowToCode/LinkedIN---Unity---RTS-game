67cd83caf3501a4e44a5ac1da3bb4eb83189b257
RomanLearnsHowToCode <47738655+RomanLearnsHowToCode@users.noreply.github.com>
2021-06-26 15:51:01 +0100

    Add files via upload

---
+++ AI/CreateDronesAi.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateDronesAi : AiBehavior {
+
+    public int DronesPerBase = 5;
+    public float Cost = 25;
+    private AiSupport support;
+
+    public override float GetWeight()
+    {
+        if (support == null)
+            support = AiSupport.GetSupport(gameObject);
+
+        if (support.Player.Credits < Cost)
+            return 0;
+
+        var drones = support.Drones.Count;
+        var bases = support.CommandBases.Count;
+
+        if (bases == 0)
+            return 0;
+
+        if (drones >= bases * DronesPerBase)
+            return 0;
+
+        return 1;
+
+    }
+
+    public override void Execute()
+    {
+        var bases = support.CommandBases;
+        var index = Random.Range(0, bases.Count);
+        var commandBase = bases[index];
+        commandBase.GetComponent<CreateUnitAction>().GetClickAction()();
+
+    }
+
+}
