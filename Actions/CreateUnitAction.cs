1a48e1f2508b7f4480a7857afb455b5b8c816a1f
RomanLearnsHowToCode <47738655+RomanLearnsHowToCode@users.noreply.github.com>
2021-06-26 15:41:26 +0100

    Add files via upload

---
+++ Actions/CreateUnitAction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateUnitAction : ActionBehavior {
+
+
+    public GameObject Prefab;
+    public float Cost = 0;
+    private PlayerSetupDefinition player;
+
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<Player>().Info;
+	}
+	
+   public override System.Action GetClickAction()
+    {
+        return delegate ()
+        {
+            if (player.Credits < Cost)
+            {
+                Debug.Log("Cannot Create, It costs " + Cost);
+                return;
+            }
+
+            var go = (GameObject)GameObject.Instantiate(
+                Prefab,
+                transform.position,
+                Quaternion.identity);
+
+            go.AddComponent<Player>().Info = player;
+            go.AddComponent<RightClickNavigation>();
+            go.AddComponent<ActionSelect>();
+            player.Credits -= Cost;
+        };
+    }     
+}
